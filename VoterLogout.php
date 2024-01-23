f2596a262a7b1529411e3510b760f2f75251e544
Navaneeth1510 <117336493+Navaneeth1510@users.noreply.github.com>
2024-01-23 19:47:04 +0530

    Add files via uploads

--- VoterLogout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_destroy();
-    echo '<script type="text/javascript">
-            window.onload = function () { 
-            window.location.href = "VoterLoginPage2.php";
-            }</script>';
-?>
